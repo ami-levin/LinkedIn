305138e2eccc8f2a9f864d76dcd50056da681b8a
Ami Levin <amilevin@gmail.com>
2020-04-06 16:33:17 -0700

    Add files via upload

---
+++ Window Functions/Chapter1/Video1.sql
@@ -0,0 +1,17 @@
+--------------------------------------
+-- LinkedIn Learning -----------------
+-- Advanced SQL - Window Functions ---
+-- Ami Levin 2020 --------------------
+-- .\Code Demos\Chapter1\Video1.sql --
+--------------------------------------
+
+-- Course materials on GitHub
+https://github.com/ami-levin/LinkedIn/tree/master/Window%20Functions
+
+-- Animal shelter project on GitHub
+https://github.com/ami-levin/Animal_Shelter
+
+-- NOTE!!! The above project page is for the generic animal shelter database project !!!
+-- For the version that is used in this training, go to
+https://github.com/ami-levin/LinkedIn/tree/master/Window%20Functions/demo%20database
+
