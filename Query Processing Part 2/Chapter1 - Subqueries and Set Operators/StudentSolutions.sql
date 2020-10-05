72f5a3b92b063abc5b6d55e605f2e0a2bdc4fad9
Ami Levin <amilevin@gmail.com>
2020-10-05 11:23:36 -0700

    Create StudentSolutions.sql

---
+++ Query Processing Part 2/Chapter1 - Subqueries and Set Operators/StudentSolutions.sql
@@ -0,0 +1,20 @@
+--------------
+-- OPTIONAL --
+--------------
+-- Name:
+-- Twitter handle:
+-- Facebook page:
+-- Website:
+-- Date:
+
+--------------
+-- Mandatory --
+--------------
+-- Solution
+
+-- <Your solution code here>
+
+-- Explanation
+
+-- <Your brief explanation here>
+
