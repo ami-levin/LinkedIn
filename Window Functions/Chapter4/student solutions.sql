993ecbfc6a1e7f3049b506035fbbbf5f31b99627
Ami Levin <amilevin@gmail.com>
2020-11-08 12:58:26 -0800

    Create student solutions.sql

---
+++ Window Functions/Chapter4/student solutions.sql
@@ -0,0 +1,24 @@
+  
+/*
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@ Copy the template headers below.          @@
+@@ Modify content for your solution.         @@
+@@ Paste below the last solution.            @@
+@@ Submit a PR.                              @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+--------------
+-- OPTIONAL --
+--------------
+-- Name: 
+-- Twitter:
+-- LinkedIn:
+-- Facebook:
+-- Website:
+-- Date: 
+
+--------------
+-- Mandatory --
+--------------
+-- Solution + explanation
