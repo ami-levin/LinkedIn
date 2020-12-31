32ce7032174b1e10bb634b87145f741fcd61d4d2
Ami Levin <amilevin@gmail.com>
2020-12-30 16:50:59 -0800

    Create Student Suggestions.sql

---
+++ Query Processing Part 2/Chapter3 - More on Grouping/Student Suggestions.sql
@@ -0,0 +1,27 @@
+/*
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@ Copy the template headers below.          @@
+@@ Modify content for your suggestion.       @@
+@@ Paste below the last solution.            @@
+@@ Submit a PR.                              @@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+--------------
+-- OPTIONAL --
+--------------
+-- Name: 
+-- Twitter:
+-- LinkedIn:
+-- Facebook:
+-- Website:
+-- Date: 
+
+--------------
+-- Mandatory --
+--------------
+-- Suggestion 
+-- explanation
+
+-----------------------------------------------
+-----------------------------------------------
